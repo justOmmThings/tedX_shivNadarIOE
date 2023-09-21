82b545712f51213e558ca2df8710ef03fe8ff71d
Omm Mishra <70828113+justOmmThings@users.noreply.github.com>
2023-09-22 03:48:10 +0530

    Add files via upload

---
+++ tedX-1/app.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Sep 24, 2023 16:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("time-left").innerHTML = "Time Left: " + days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+}, 1000);
